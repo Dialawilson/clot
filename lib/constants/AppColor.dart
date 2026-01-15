665d55b28fbbe7c6afae4c04b6e26aa6de335d5f
Diala wilson <diala.i.wilson@gmail.com>
2026-01-15 15:31:42 +0100

    static appcolor

---
+++ lib/constants/AppColor.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+class AppColor {
+  AppColor._();
+
+// background color
+  static const Color backgroundColor = Color(0xFFFFFFFF);
+  // btn background color
+  static const Color btnBackgroundColor = Color(0xFF8E6CEF);
+  static const Color btnTextColor = Color(0xFFFFFFFF);
+
+  
+  // input placeholder 
+  static const Color inputPlaceholder = Color(0xFF272727);
+
+  // input BG
+  static const Color inputBG = Color(0xFFF4F4F4);
+// text
+static const Color textColor = Color(0xFF272727);
+}
